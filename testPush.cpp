2365604f73dd401d3998e5031cdab176a35dbf00
DnYAlv <denny.ginting@binus.ac.id>
2021-05-11 12:07:11 +0700

    added while loop

--- testPush.cpp
+++ testPush.cpp
@@ -4,5 +4,10 @@ int main(){
   puts("Hello World");
   printf ("Test 1");
   for(int i = 0; i < 10 ; i++) puts("hey");
+  int i = 0;
+  while(i < 10){
+    printf ("Hello 1");
+    i++;
+  }
   return 0;
 }
